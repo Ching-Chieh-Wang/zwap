f418bf030b3ee794b41e96991fcd37bbe51a8ff3
ching-chieh-wang <immactavish@gmail.com>
2025-05-07 19:21:25 -0400

    fix: build failed

--- eureka-service/src/main/java/com/zwap/eureka_service/eureka_service/config/SecurityConfig.java
+++ eureka-service/src/main/java/com/zwap/eureka_service/eureka_service/config/SecurityConfig.java
@@ -3,16 +3,9 @@ package com.zwap.eureka_service.eureka_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
