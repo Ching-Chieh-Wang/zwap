88db2626857bf9f470f82bde97d2c990ed18485c
ching-chieh-wang <immactavish@gmail.com>
2025-05-11 14:48:13 -0400

    fix: build failed

---
+++ config/src/main/java/com/zwap/config/service/PingService.java
@@ -0,0 +1,31 @@
+package com.zwap.config.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class PingService {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @Value("${zwap.ping.clients}")
+    private List<String> clientUrls;
+
+    @Scheduled(fixedRate = 10000)
+    public void pingClients() {
+        for (String url : clientUrls) {
+            try {
+                restTemplate.postForEntity(url, null, String.class);
+                log.info("Successfully pinged {}", url);
+            } catch (Exception ex) {
+                log.warn("Failed to contact {}", url, ex);
+            }
+        }
+    }
+}
