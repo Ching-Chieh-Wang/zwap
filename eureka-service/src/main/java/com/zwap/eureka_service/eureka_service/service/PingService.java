92bf2e7c4412a022524ebe5dfc0f1a89edf514e1
ching-chieh-wang <immactavish@gmail.com>
2025-05-12 13:11:50 -0400

    fix: build failed

--- eureka-service/src/main/java/com/zwap/eureka_service/eureka_service/service/PingService.java
+++ eureka-service/src/main/java/com/zwap/eureka_service/eureka_service/service/PingService.java
@@ -3,26 +3,35 @@ package com.zwap.eureka_service.eureka_service.service;
 import com.zwap.eureka_service.eureka_service.properties.PingProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class PingService {
 
-    private final PingProperties pingProperties;
+    private final DiscoveryClient discoveryClient;
     private final RestTemplate restTemplate = new RestTemplate();
 
     @Scheduled(fixedRate = 600000)
     public void pingClients() {
-        for (String url : pingProperties.getClients()) {
-            try {
-                restTemplate.postForEntity(url, null, String.class);
-                log.info("Successfully pinged {}", url);
-            } catch (Exception ex) {
-                log.warn("Failed to contact {}", url, ex);
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            List<ServiceInstance> instances = discoveryClient.getInstances(service);
+            for (ServiceInstance instance : instances) {
+                String url = instance.getUri().toString();
+                try {
+                    restTemplate.postForEntity(url + "/ping", null, String.class);
+                    log.info("Successfully pinged {}", url);
+                } catch (Exception ex) {
+                    log.warn("Failed to contact {}", url, ex);
+                }
             }
         }
     }
