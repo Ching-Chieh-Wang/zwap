9de660ff29423a5983ffa2672fc8d0838e2ff122
Ching-Chieh-Wang <immactavish@gmail.com>
2025-07-01 16:33:27 -0400

    fix: allow writable logs dir

--- services/kafka/run_connector.sh
+++ services/kafka/run_connector.sh
@@ -2,5 +2,5 @@
 
 apptainer run \
   --bind $PWD/connect.offsets:/opt/bitnami/kafka/connect.offsets \
-  --bind $PWD/logs:/opt/bitnami/kafka/logs
+  --bind $PWD/logs:/opt/bitnami/kafka/logs \
   kafka.sif connector
