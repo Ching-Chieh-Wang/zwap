41b35c7444e381dfde6bf18db5e30c1072f2ef54
Ching-Chieh-Wang <immactavish@gmail.com>
2025-07-02 15:05:36 -0400

    fix: es plugin config

--- services/kafka/run_connector.sh
+++ services/kafka/run_connector.sh
@@ -1,5 +1,5 @@
 apptainer run \
   --bind $PWD/connect.offsets:/opt/bitnami/kafka/connect.offsets \
   --bind $PWD/logs:/opt/bitnami/kafka/logs \
-  --bind /home/immactavish/elasticsearch.jks:/elasticsearch.jks \
+  --bind $PWD/elasticsearch.jks:/elasticsearch.jks \
   kafka.sif connector
