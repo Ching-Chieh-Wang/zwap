9085ad7a14c876e6749aeb612daab515de3cf756
Ching-Chieh-Wang <immactavish@gmail.com>
2025-07-31 19:22:01 -0400

    feat: update setup script for MongoDB and Redis connectors

--- services/kafka/setup.sh
+++ services/kafka/setup.sh
@@ -16,7 +16,8 @@ fi
 echo "[+] Cleaning up logs, meta, and plugins directories..."
 rm -rf logs/* meta/* plugins/*
 mkdir -p logs meta \
-         plugins/debezium-mongodb \
+         plugins/product-mongodb=sink-connector \
+         plugins/product-mongodb-source-connector \
          plugins/redis-sink
 rm -f connect.offsets
 touch connect.offsets
@@ -47,32 +48,33 @@ envsubst < config/elasticsearch-connector-template.properties \
 
 echo "[+] All templates resolved."
 
-echo "[+] Downloading Debezium MongoDB connector (3.1.2.Final)..."
-curl -sSL -o debezium-mongodb.tar.gz \
+echo "[+] Downloading Debezium MongoDB source connector (3.1.2.Final)..."
+curl -sSL -o product-mongodb-source-connector.tar.gz \
   https://repo1.maven.org/maven2/io/debezium/debezium-connector-mongodb/3.1.2.Final/debezium-connector-mongodb-3.1.2.Final-plugin.tar.gz
 
-mkdir -p tmp-debezium
-tar -xzf debezium-mongodb.tar.gz -C tmp-debezium
-cp tmp-debezium/debezium-connector-mongodb/*.jar \
-   plugins/debezium-mongodb/
-rm -rf tmp-debezium debezium-mongodb.tar.gz
+mkdir -p tmp-product-mongodb-source-connector
+tar -xzf product-mongodb-source-connector.tar.gz -C product-mongodb-source-connector
+cp product-mongodb-source-connector/debezium-connector-mongodb/*.jar \
+   plugins/product-mongodb-source-connector/
+rm -rf tmp-product-mongodb-source-connector product-mongodb-source-connector.tar.gz
 echo "[+] Debezium MongoDB connector JARs copied to plugins/debezium-mongodb."
 
-echo "[+] Cloning Redis Kafka Connect repo with tag v7.4..."
-git clone --branch v7.4 --depth 1 https://github.com/redis-field-engineering/redis-kafka-connect.git tmp-redis-sink/redis-kafka-connect
+echo "[+] Downloading MongoDB Kafka sink connector (2.0.0)..."
+curl -sSL -o mongodb-sink-connector.zip \
+  https://hub-downloads.confluent.io/api/plugins/mongodb/kafka-connect-mongodb/versions/2.0.0/mongodb-kafka-connect-mongodb-2.0.0.zip
 
-echo "[+] Building Redis Kafka Connect connector..."
-cd tmp-redis-sink/redis-kafka-connect
-mvn clean package -Dmaven.test.skip=true
-cd ../../
+mkdir -p tmp-mongodb-sink-connector
+unzip -qo mongodb-sink-connector.zip -d tmp-mongodb-sink-connector
+cp tmp-mongodb-sink-connector/mongodb-kafka-connect-mongodb-2.0.0/lib/*.jar \
+   plugins/mongodb-sink-connector/
 
-echo "[+] Copying all connector JARs to plugins/redis-sink..."
+
+echo "[+] Downloading pre-built Redis Kafka Connect connector (v7.4)..."
 mkdir -p plugins/redis-sink
-cp tmp-redis-sink/redis-kafka-connect/target/*.jar \
-   plugins/redis-sink/ ;
+curl -sSL -o plugins/redis-sink/redis-kafka-connect-7.4-SNAPSHOT.jar \
+  https://github.com/Ching-Chieh-Wang/redis-kafka-connect/releases/download/v7.4/redis-kafka-connect-7.4-SNAPSHOT.jar
+echo "[+] Redis Kafka Connect JAR downloaded to plugins/redis-sink."
 
-rm -rf tmp-redis-sink
-echo "[+] Redis Kafka Connect JARs copied to kafka/plugins/redis-sink."
 
 echo "[+] Downloading Elasticsearch sink connector (15.0.0)..."
 curl -sSL -o elasticsearch.zip \
