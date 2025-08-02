16dc1401299fe7e58ab8754515f02919ff5673f9
Ching-Chieh-Wang <immactavish@gmail.com>
2025-08-02 19:43:29 -0400

    feat: update ProductConverter to use Query DTOs for entity conversion

--- product-write-service/src/main/java/com/zwap/product_write_service/product_write_service/converter/ProductConverter.java
+++ product-write-service/src/main/java/com/zwap/product_write_service/product_write_service/converter/ProductConverter.java
@@ -1,23 +1,23 @@
 package com.zwap.product_write_service.product_write_service.converter;
 
 import com.zwap.product_common.product_common.entity.Product;
-import com.zwap.product_write_service.product_write_service.dto.ProductCreateDTO;
-import com.zwap.product_write_service.product_write_service.dto.ProductUpdateDTO;
+import com.zwap.product_write_service.product_write_service.dto.ProductCreateQry;
+import com.zwap.product_write_service.product_write_service.dto.ProductUpdateQry;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProductConverter {
-    public static Product toEntity(ProductCreateDTO dto, String userId) {
+    public static Product toEntity(ProductCreateQry qry, String userId) {
         Product product = new Product();
-        BeanUtils.copyProperties(dto, product);
+        BeanUtils.copyProperties(qry, product);
         product.setUserId(userId);
         return product;
     }
 
-    public static Product toEntity(ProductUpdateDTO dto) {
+    public static Product toEntity(ProductUpdateQry qry) {
         Product product = new Product();
-        BeanUtils.copyProperties(dto, product);
+        BeanUtils.copyProperties(qry, product);
         return product;
     }
 }
